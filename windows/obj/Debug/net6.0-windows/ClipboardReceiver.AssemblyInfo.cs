6e3bb2ef028ba81a9f232f03f5adcf6922f62bba
Finlay123456 <fcooper4@uwo.ca>
2025-09-05 11:46:53 -0400

    idk

--- windows/obj/Debug/net6.0-windows/ClipboardReceiver.AssemblyInfo.cs
+++ windows/obj/Debug/net6.0-windows/ClipboardReceiver.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ClipboardReceiver")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cd5752f14923c3c119aef7ca23a2e55389a7a5b9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1b998e28c981dfcb891340d561b901cc5da2aa52")]
 [assembly: System.Reflection.AssemblyProductAttribute("ClipboardReceiver")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ClipboardReceiver")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
